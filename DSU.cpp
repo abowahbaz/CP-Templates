b50139eb32032193a6f5e9dd12d5ad58b5577bc9
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2024-11-21 20:37:02 +0200

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,72 +1,60 @@
-//Yet another messy code by Abo_WahbaZ
 #include <bits/stdc++.h>
-#define nl "\n"
-#define sz(st) int(st.size())
-#define all(st) st.begin(), st.end()
 using namespace std;
 using ll = long long;
+#define sz(st) int(st.size())
+#define all(st) st.begin(), st.end()
 
-class DisjointSetUnion
+class DSU
 {
-    private:
-    static constexpr int MAXN = 2e5 + 10;
-    int parent[MAXN], Ssize[MAXN];
-    int n = 1, sets = 1;
-    int findSet(int node)
-    {
-        if (parent[node] == node) return node;
-        return parent[node] = findSet(parent[node]);
-    }
-    public:
-    DisjointSetUnion() {}
-    DisjointSetUnion(int N)
-    {
-        n = sets = N;
-        for (int i = 1;i <= n;i++)
-            parent[i] = i, Ssize[i] = 1;
-    }
-    void joinSets(int u, int v)
-    {
-        u = findSet(u), v = findSet(v);
-        if (u == v) return;
-        if (Ssize[u] < Ssize[v])
-            swap(u, v);
-        Ssize[u] += Ssize[v];
-        parent[v] = u;
-        sets--;
-    }
-    bool isSameSet(int u, int v)
-    {
-        return findSet(u) == findSet(v);
-    }
-    int numberOfSets()
-    {
-        return sets;
-    }
-    int getSize(int node)
-    {
-        return Ssize[findSet(node)];
-    }
+public:
+	DSU(int n) : sets(n)
+	{
+		parent = gsize = vector < int >(n + 1, 0);
+		for (int i = 1; i <= n; i++)
+			parent[i] = i;
+	}
+	int Find(int u)
+	{
+		return parent[u] == u ? u : parent[u] = Find(parent[u]);
+	}
+	void Union(int u, int v)
+	{
+		u = Find(u), v = Find(v);
+		if (u == v) return;
+		if (gsize[u] < gsize[v]) swap(u, v);
+		parent[v] = u;
+		gsize[u] += gsize[v];
+		sets--;
+	}
+	int getSets()
+	{
+		return sets;
+	}
+private:
+	vector < int > parent, gsize;
+	int sets;
 
 };
 
 
-void Solve(int tc)
+void Solve()
 {
+
 }
 
-signed main()
+int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr), cout.tie(nullptr);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+#if LOCAL
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
-    int t = 1;
-    //cin >> t;
-    cout << fixed << setprecision(12);
-    for (int tc = 1; tc <= t; tc++)
-        Solve(tc);
-    return 0;
-};
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	//cin >> t;
+	for (int tc = 1; tc <= t; tc++) {
+		Solve();
+		cout << "\n";
+	}
+	return 0;
+}
