84801d290c66c8273d5c8adbb42fad9d07630e79
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2024-11-22 00:46:29 +0200

    Update MO_Algorithm.cpp

--- MO_Algorithm.cpp
+++ MO_Algorithm.cpp
@@ -4,63 +4,72 @@ using ll = long long;
 #define sz(st) int(st.size())
 #define all(st) st.begin(), st.end()
 
-template < typename T > class Mo
+template < typename T > struct Mo
 {
-    public:
-    static constexpr int block_size = 350; // TODO
-    struct Query
-    {
-        int l, r, idx;
-        bool operator < (const Query& q) const
-        {
-            if (l / block_size != q.l / block_size)
-                return l / block_size < q.l / block_size;
-            return r < q.r;
-        }
-        Query(int l, int r, int idx) : l(l), r(r), idx(idx) {}
-    };
-    void push_query(int l, int r, int idx)
-    {
-        queries[idx] = Query(l, r, idx);
-    }
-    Mo(int Q, const vector < T >& nums = {})
-    {
-        queries.resize(Q);
-        ans.resize(Q);
-    }
+public:
+	static constexpr int block_size = 450; // TODO
+	struct Query
+	{
+		int l, r, idx;
+		bool operator < (const Query& q) const
+		{
+			if (l / block_size != q.l / block_size)
+				return l / block_size < q.l / block_size;
+			return ((l / block_size & 1) ^ (r < q.r));
+		}
+		Query(int l, int r, int idx) : l(l), r(r), idx(idx) {}
+		Query() {};
+	};
+	void Set_Query(int l, int r, int idx)
+	{
+		queries[idx] = Query(l, r, idx);
+	}
 
-    void process()
-    {
-        sort(all(queries));
-        for (Query& q : queries)
-        {
-            while (L > q.l)
-                add(--L);
-            while (R < q.r)
-                add(++R);
-            while (L < q.l)
-                remove(L++);
-            while (R > q.r)
-                remove(R--);
-            ans[q.idx] = get_answer();
-        }
+	Mo(int Q, const vector < T >& arr = {})
+	{
+		answers.resize(Q);
+		queries.resize(Q);
+		nums = arr;
 
-    }
-    private:
-    int L = 1, R = 0;
-    void add(T x) // TODO
-    {
-    }
-    void remove(T x) // TODO
-    {
-    }
+	}
+	void Process()
+	{
+		sort(queries.begin(), queries.end());
+		for (Query& q : queries)
+		{
+			while (L > q.l)
+				Add(--L);
+			while (R < q.r)
+				Add(++R);
+			while (L < q.l)
+				Remove(L++);
+			while (R > q.r)
+				Remove(R--);
+			answers[q.idx] = Get_Answer();
+		}
 
-    T get_answer() // TODO
-    {
-        return 0;
-    }
-    vector < Query > queries;
-    vector < T > ans;
+	}
+	vector < T > answers;
+
+private:
+	int L = 1, R = 0;
+	T answer = 0; // TODO
+	vector < T > nums;
+	void Add(int idx) // TODO
+	{
+
+	}
+
+	void Remove(int idx) // TODO
+	{
+
+	}
+
+	T Get_Answer() // TODO
+	{
+		return answer;
+	}
+	vector < Query > queries;
 
 };
 
@@ -74,16 +83,17 @@ void Solve()
 int main()
 {
 #if LOCAL
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t = 1;
-    //cin >> t;
-    for (int tc = 1; tc <= t; tc++) {
-        Solve();
-        cout << "\n";
-    }
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+	//cin >> t;
+	for (int tc = 1; tc <= t; tc++) {
+		Solve();
+		cout << "\n";
+	}
+	cerr << clock() * 1.0 / CLOCKS_PER_SEC << "\n";
+	return 0;
 }
