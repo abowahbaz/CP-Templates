2a8b3d3fcd53483c0ca1a88ebe1779384713627d
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2025-01-16 17:57:14 +0200

    Update Coordinate_Compression.cpp

--- Others/Coordinate_Compression.cpp
+++ Others/Coordinate_Compression.cpp
@@ -4,15 +4,14 @@ using ll = long long;
 #define sz(st) int(st.size())
 #define all(st) st.begin(), st.end()
 
-template<typename T>
-vector<T> compress(const vector<T> arr)
+vector <int> compress(const vector<T> arr) // compress [int,long long,string]
 {
     vector<T> temp = arr;
     sort(temp.begin(), temp.end());
     temp.erase(unique(all(temp)), temp.end());
-    vector<T> ret;
+    vector<int> ret;
     for (int i = 0; i < arr.size(); i++) {
-        T new_val = upper_bound(all(temp), arr[i]) - temp.begin();
+        int new_val = upper_bound(all(temp), arr[i]) - temp.begin();
         ret.emplace_back(new_val);
     }
     return ret;
