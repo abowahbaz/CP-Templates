433d17c9d867cbdc3fd4f0edf070546582a545f7
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2025-01-16 17:55:26 +0200

    Update Sieve.cpp

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -18,9 +18,9 @@ vector < bool > sieve(int MAXN)
     return is_prime;
 }
 
-vector < ll > spf(int MAXN) // smallest prime factor
+vector < int > spf(int MAXN) // smallest prime factor
 {
-    vector < ll > spf(MAXN + 1);
+    vector < int > spf(MAXN + 1);
     for (int i = 1; i <= MAXN; i++) {
         spf[i] = i;
     }
@@ -40,10 +40,13 @@ vector < ll > prime_factors(ll n)
 {
     vector < ll > factors;
     for (ll i = 2; i * i <= n; i++) {
+        int cnt = 0;
         while (n % i == 0) {
-            factors.push_back(i);
+            cnt++;
             n /= i;
         }
+        if(cnt)
+            factors.push_back(i);
     }
     if (n > 1) {
         factors.push_back(n);
