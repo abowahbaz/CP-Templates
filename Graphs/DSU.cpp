36e43ca2748bc58320a01bcc287b6010037008d8
abowahbaz <ahmedz.mohameda69@gmail.com>
2025-02-09 23:41:36 +0200

    added same function

--- Graphs/DSU.cpp
+++ Graphs/DSU.cpp
@@ -12,43 +12,40 @@ class DSU
 	public:
 	DSU(int n) : sets(n)
 	{
-		parent = gsize = vector < int >(n + 1, 0);
+		parent = gsize = vector < int >(n + 1, 1);
 		for (int i = 1; i <= n; i++)
 			parent[i] = i;
 	}
-	int get_parent(int u)
+	int find(int u)
 	{
-		return parent[u] == u ? u : parent[u] = get_parent(parent[u]);
+		return parent[u] == u ? u : parent[u] = find(parent[u]);
 	}
 	void join(int u, int v)
 	{
-		u = get_parent(u), v = get_parent(v);
+		u = find(u), v = find(v);
 		if (u == v) return;
 		if (gsize[u] < gsize[v]) swap(u, v);
 		parent[v] = u;
 		gsize[u] += gsize[v];
 		sets--;
 	}
-	int getSets()
-	{
-		return sets;
-	}
-
+	int get_sets() { return sets; }
+	bool same(int u, int v) { return find(u) == find(v); }
 };
 
 
-void Solve()
+void solve()
 {
 }
 
 signed main()
 {
- 	ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	int t = 1;
 	//cin >> t;
 	for (int tc = 1; tc <= t; tc++) {
-		Solve();
+		solve();
 		cout << "\n";
 	}
 	return 0;
